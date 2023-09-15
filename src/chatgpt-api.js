45a731738662ac04daa166ab92624633714fda64
Irving Lop <irving.dev.mobi@gmail.com>
2023-09-15 13:15:53 -0600

    setup to improve result for the flutter sura team.

--- src/chatgpt-api.js
+++ src/chatgpt-api.js
@@ -10,8 +10,16 @@ const openai = new OpenAI({
 });
 
 module.exports = {
-  async contentMsg(framework, code) {
-    return "Could you generate unit test in " + framework +" Language, showing at least two unit tests per function, containing relevant assertions and all required packages within a single Java class? The source code is the following: " + code;
+  async contentMsg(framework, code, testExample, repositoryDefinition) {
+    return "Could you generate a unit tests class in " + framework +" language for the 'run' function?"
+        + "Showing at least two unit tests per function, containing relevant cases when the 'store' object dispatches an action, "
+        + "use GenerateNiceMocks instead of GenerateMocks for dependencies."
+        + "and please DON'T use argument matcher any or anyNamed, create a fake instance of the arguments instead."
+        + "The source code is the following: " + code
+        + " Here is an example of the expected structure for the unit tests: " + testExample
+        // If we can use gpt-4 we can even send the repository definition. If not, comment out the next line.
+        + " And here is the definition of the repository: " + repositoryDefinition
+        ;
   },
 
   async readFileAsCode(filePath, file) {
@@ -31,15 +39,15 @@ module.exports = {
 
     try {
       const response = await openai.chat.completions.create({
-        model: "gpt-3.5-turbo",
+        model: "gpt-4",
         messages: [
           {
             role: "user",
             content: prompt,
           },
         ],
-        temperature: 0,
-        max_tokens: 2048,
+        temperature: 0.2,
+        max_tokens: 4097,
       });
 
       const { message } = response.choices[0];
@@ -54,7 +62,8 @@ module.exports = {
   async createTestSuitFile(file, output) {
     //path where unit test file is created
     const outputFolder = "./output/";
-    let fileName = path.join(outputFolder + "UT" + file);
+    const testFile = file.replace(/(\.dart)$/, '_test$1');
+    let fileName = path.join(outputFolder + testFile);
     try {
       fs.writeFileSync(fileName, output);
       console.log(`Message written to file: ${fileName}`);
--- src/generate.js
+++ src/generate.js
@@ -5,15 +5,20 @@ const {
   createTestSuitFile,
 } = require("./chatgpt-api");
 
-const path =
-  "../../users-be/src/main/java/com/speedcast/sigma/users/usermanagement/controllers/";
-const file = "RolesController.java";
-const framework = 'Java';
+const framework = 'Dart';
+const path = "../../1065-relevant_superapp_frontend-fr/modules/rlv_health/lib/domain/middlewares/";
+const file = "get_health_home_menu_items_enabled_map_middleware.dart";
+const testPathExample = "../../1065-relevant_superapp_frontend-fr/modules/rlv_health/test/domain/middlewares/";
+const fileExample = "answer_hada_question_middleware_test.dart";
+const repositoryPath = "../../1065-relevant_superapp_frontend-fr/modules/rlv_health/lib/domain/repositories/";
+const repositoryFile = "health_repository.dart";
 
 (async () => {
   try {
     const code = await readFileAsCode(path, file);
-    let prompt = await contentMsg(framework, code);
+    const testExample = await readFileAsCode(testPathExample, fileExample);
+    const repositoryDefinition = await readFileAsCode(repositoryPath, repositoryFile);
+    let prompt = await contentMsg(framework, code, testExample, repositoryDefinition);
     let output = await generateUnitTest(prompt);
     await createTestSuitFile(file, output);
   } catch (error) {
